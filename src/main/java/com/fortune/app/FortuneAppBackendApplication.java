d936e5065c7815e91e963af2961ee99cfc974af6
dev-everyday <everyharus@gmail.com>
2025-01-20 17:54:45 +0900

    feat: add main application entry point

---
+++ src/main/java/com/fortune/app/FortuneAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fortune.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortuneAppBackendApplication.class, args);
+	}
+
+}
