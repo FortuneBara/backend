ab11ed8717d6b4c515105a580a7ef2d99da00833
dev-everyday <everyharus@gmail.com>
2025-01-20 21:41:29 +0900

    chore: modify RequestMapping address

--- src/main/java/com/fortune/app/card/controller/CardController.java
+++ src/main/java/com/fortune/app/card/controller/CardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/card")
+@RequestMapping("/card")
 public class CardController {
 
     private final CardService cardService;
--- src/main/java/com/fortune/app/daily/controller/CardLogController.java
+++ src/main/java/com/fortune/app/daily/controller/CardLogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cardLog")
+@RequestMapping("/cardLog")
 public class CardLogController {
 
     private final CardLogService cardLogService;
