78552383f51d5b959c2dc1940f285282d9406e3a
dev-everyday <everyharus@gmail.com>
2025-02-09 14:33:12 +0900

    refactor: add Component annotation

--- src/main/java/com/fortune/app/oauth/handler/OAuth2LogoutSuccessHandler.java
+++ src/main/java/com/fortune/app/oauth/handler/OAuth2LogoutSuccessHandler.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 
+@Component
 public class OAuth2LogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
