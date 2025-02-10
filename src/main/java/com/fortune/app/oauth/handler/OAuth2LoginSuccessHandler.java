220a9be48035b5cbc3b0603daf8745543f96603d
dev-everyday <everyharus@gmail.com>
2025-02-10 14:42:03 +0900

    chore: add front_url properties and modify redirect url

--- src/main/java/com/fortune/app/oauth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/fortune/app/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -6,6 +6,7 @@ import com.fortune.app.user.repository.UserRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -16,6 +17,8 @@ import java.util.Optional;
 
 @Component
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+    @Value("${frontend.url:http://localhost:3000}")
+    private String frontendUrl;
 
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
@@ -29,7 +32,6 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
-        String frontendUrl = System.getenv("FRONTEND_URL") != null ? System.getenv("FRONTEND_URL") : "http://localhost:3000";
 
         Optional<User> userOptional = userRepository.findByEmailQueryDSL(email);
         if (userOptional.isPresent()) {
